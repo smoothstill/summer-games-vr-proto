87d261009e6fb65447bd0e3a25bc6cc6a4d42d81
smoothstill <59178987+smoothstill@users.noreply.github.com>
2022-06-29 01:18:58 +0300

    hand physics step 1

--- Assets/Oculus Hands Physics/Scripts/HandPresence.cs
+++ Assets/Oculus Hands Physics/Scripts/HandPresence.cs
@@ -1,18 +1,14 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 
 public class HandPresence : MonoBehaviour
 {
-
-    public InputDeviceCharacteristics controllerCharacteristics;
-    public GameObject handModelPrefab;
+    public InputDeviceCharacteristics controllerCharacteristics;    
     private InputDevice targetDevice;
-    private GameObject spawnedHandModel;
-    private Animator handAnimator;
+    public Animator handAnimator;
 
-    // Start is called before the first frame update
     void Start()
     {
         TryInitialize();
@@ -21,19 +17,17 @@ public class HandPresence : MonoBehaviour
     void TryInitialize()
     {
         List<InputDevice> devices = new List<InputDevice>();
-        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
 
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
         if (devices.Count > 0)
         {
             targetDevice = devices[0];
-            spawnedHandModel = Instantiate(handModelPrefab, transform);
-            handAnimator = spawnedHandModel.GetComponent<Animator>();
         }
     }
 
     void UpdateHandAnimation()
     {
-        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        if(targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
         {
             handAnimator.SetFloat("Trigger", triggerValue);
         }
@@ -55,11 +49,11 @@ public class HandPresence : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!targetDevice.isValid)
+        if(!targetDevice.isValid)
         {
             TryInitialize();
         }
-        else 
+        else
         {
             UpdateHandAnimation();
         }
