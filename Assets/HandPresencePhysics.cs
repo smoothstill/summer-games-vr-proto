ed8ae5be0017962f21f0929a771bde75e227d7b7
smoothstill <59178987+smoothstill@users.noreply.github.com>
2022-06-29 22:59:50 +0300

    improved hand interaction physics and added teleportation interaction layer

--- Assets/HandPresencePhysics.cs
+++ Assets/HandPresencePhysics.cs
@@ -9,11 +9,34 @@ public class HandPresencePhysics : MonoBehaviour
     private Rigidbody rb;
     public Renderer nonPhysicalHand;
     public float showNonPhysicalHandDistance = 0.05f;
+    private Collider[] handColliders;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        handColliders = GetComponentsInChildren<Collider>();
+    }
+
+    public void EnableHandCollider()
+    {
+        foreach (var item in handColliders)
+        {
+            item.enabled = true;
+        }
+    }
+
+    public void EnableHandColliderDelay(float delay)
+    {
+        Invoke("EnableHandCollider", delay);
+    }
+
+    public void DisableHandCollider()
+    {
+        foreach (var item in handColliders)
+        {
+            item.enabled = false;
+        }
     }
 
     private void Update()
